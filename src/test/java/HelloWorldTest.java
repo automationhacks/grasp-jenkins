f9517b76b92e56435da1b98f180ce0a97a6a157d
automationhacks <automationhacks@gmail.com>
2023-08-06 21:06:33 +0530

    Let's add a test to make things interesting.

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class HelloWorldTest {
+    @Test
+    public void aTest() {
+        // duh! 🤦‍♂️
+        assertTrue(true);
+
+    }
+}
